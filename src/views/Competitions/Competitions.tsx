4393b1cfead765f20d17d8e5df1f3ff03f681eb6
Saurabh Burade <sburade3@gmail.com>
2023-12-09 13:18:06 +0000

    init: competitions

---
+++ src/views/Competitions/Competitions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/Card";
+
+type Props = {};
+
+function Competitions({}: Props) {
+    return <div className=""><Card/>
+    
+    <Card/><Card/><Card/><Card/><Card/><Card/>
+    </div>;
+}
+
+export default Competitions;
