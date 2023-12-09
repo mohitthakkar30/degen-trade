c09a67c2c048f04a654cc88a904aff076b4f2d10
Saurabh Burade <sburade3@gmail.com>
2023-12-09 12:34:46 +0000

    feat: folder str setup

---
+++ src/components/Header/WalletConnector.tsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+
+type Props = {};
+
+function WalletConnector({}: Props) {
+    return (
+        <div>
+            <ConnectButton />
+        </div>
+    );
+}
+
+export default WalletConnector;
