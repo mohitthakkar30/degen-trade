8756ab6a745297c8c790b4f38d7716ba510db432
Mohit Thakkar <thakkarmohit90@gmail.com>
2023-12-10 06:49:48 +0530

    fix: build issue

--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -13,8 +13,6 @@ type Props = {
 const trade = (props: Props) => {
 
     const address = "0x261a85ecb4cdd0c1c3e0978f833c06c8a2252d77"
-    const data = useTradingCompTime(address);
-    console.log(data)
 
 
 
